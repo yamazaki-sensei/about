b702d7d030b7fdae2c939390f95dd60a4d6431a1
hira <ohno@toreta.in>
2020-02-05 01:18:58 +0900

    Update sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
-const IndexPage = ({ userAgent }) => (
-  <h1>Hello world! - userAgent: {userAgent}</h1>
+import { NextPage } from "next";
+
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
+  <h1> Hello world! - user agent: {userAgent}</h1>
 );
 
-IndexPage.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers["user-agent"] : navigator.userAgent;
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+
   return { userAgent };
 };
 
-export default IndexPage;
+export default Home;
